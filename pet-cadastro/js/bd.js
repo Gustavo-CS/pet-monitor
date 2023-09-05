2125961b154d38c49dfd0f8873dca37d60769632
Gustavo-CS <gustavocarvalho9340@gmail.com>
2023-09-05 11:10:13 -0300

    Revert "subindo as mudanças"
    This reverts commit 7f8e4262152b7a93640d40db5d3618338591bb22.

--- pet-cadastro/js/bd.js
+++ pet-cadastro/js/bd.js
@@ -1,5 +1,3 @@
-//import {dataa} from '../../firebase.js';
-const { dataa } = require('../../firebase.js');
 
 let loginForm = document.getElementById('loginForm');
 
@@ -9,10 +7,9 @@ loginForm.addEventListener("submit", evento => {
     // const data = Object.fromEntries(loginForm);
 
     //console.log(data);
-    // const url = 'https://projeto-2-teste-c9255-default-rtdb.firebaseio.com/.json?auth=KBEH3eEdZNflAwEKNvftlyoDuDh2';
-    //var emailbd = email;
+    const url = 'https://projeto-2-teste-c9255-default-rtdb.firebaseio.com/.json';
 
-
+    
 
     let email = document.getElementById("email").value;
     console.log(email);
@@ -24,88 +21,46 @@ loginForm.addEventListener("submit", evento => {
     console.log(endereco);
     let nome = document.getElementById("nome").value;
     console.log(nome);
-    const url = 'https://projeto-2-teste-c9255-default-rtdb.firebaseio.com/teste/.json';
 
-
-    // let idade = document.getElementById("idade").value;
-    // console.log(idade);
-    // let peso = document.getElementById("peso").value;
-    // console.log(peso);
-    // let comprimento = document.getElementById("comprimento").value;
-    // console.log(comprimento);
-    // let cor_primaria = document.getElementById("cor-primaria").value;
-    // console.log(cor_primaria);
-    // let nome_pet = document.getElementById("nome-pet").value;
-    // console.log(nome_pet);
-    // let comportamento = document.getElementById("comportamento").value;
-    // console.log(comportamento);
+    let idade = document.getElementById("idade").value;
+    console.log(idade);
+    let peso = document.getElementById("peso").value;
+    console.log(peso);
+    let comprimento = document.getElementById("comprimento").value;
+    console.log(comprimento);
+    let cor_primaria = document.getElementById("cor-primaria").value;
+    console.log(cor_primaria);
+    let nome_pet = document.getElementById("nome-pet").value;
+    console.log(nome_pet);
+    let comportamento = document.getElementById("comportamento").value;
+    console.log(comportamento);
 
     let data = {
-        // name: "cu",
-        email: {
+        dono: {
             email: email,
             cpf: cpf,
             tel: tel,
             endereco: endereco,
             nome: nome,
         },
-        // dono: {
-        //     email: 'email',
-        //     cpf: 'cpf',
-        //     tel: 'tel',
-        //     endereco: 'endereco',
-        //     nome: 'nome',
-        // },
-        // pet: {
-        //     idade: idade,
-        //     peso: peso,
-        //     comprimento: comprimento,
-        //     cor_primaria: cor_primaria,
-        //     nome_pet: nome_pet,
-        //     comportamento: comportamento,
-        // }
+        pet: {
+            idade: idade,
+            peso: peso,
+            comprimento: comprimento,
+            cor_primaria: cor_primaria,
+            nome_pet: nome_pet,
+            comportamento: comportamento,
+        }
 
-    };
+    }
 
-    // var obj = {
-    //     dono: {
-    //         email: email,
-    //         cpf: cpf,
-    //         tel: tel,
-    //         endereco: endereco,
-    //         nome: nome,
-    //     },
-    //     pet: {
-    //         idade: idade,
-    //         peso: peso,
-    //         comprimento: comprimento,
-    //         cor_primaria: cor_primaria,
-    //         nome_pet: nome_pet,
-    //         comportamento: comportamento,
-    //     }
-    // };
-
-
-    request = new Request(url, {
+    var request = new Request(url, {
         method: 'POST',
-        // method: 'PATCH',
         body: JSON.stringify(data),
-        // body: data,
-        headers: new Headers(),
-
+        headers: new Headers()
     });
-    // console.log(request);
-    // request.body.locked = false;
-    // resposta = JSON.parse(request.body);
-    // console.log(resposta);
-
-    // var request = new XMLHttpRequest();
-    // request.open("POST", url, true, 'teste', 'queisso');
-    // request.setRequestHeader('Content-Type', 'application/json');
-    // request.send(JSON.stringify(data));
 
     fetch(request)
         .then(res => res.json()).then(data => console.log(data)
         )
-    console.log(dataa['localId']);
 });
